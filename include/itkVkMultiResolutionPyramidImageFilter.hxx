045ded38118afb2716529a590844402c905bcb50
Tom Birdsong <tom.birdsong@kitware.com>
2022-04-25 12:53:06 -0400

    WIP: Add VkMultiResolutionPyramidImageFilter

---
+++ include/itkVkMultiResolutionPyramidImageFilter.hxx
@@ -0,0 +1,214 @@
+/*=========================================================================
+ *
+ *  Copyright NumFOCUS
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+#ifndef itkVkMultiResolutionPyramidImageFilter_hxx
+#define itkVkMultiResolutionPyramidImageFilter_hxx
+
+#include "itkCastImageFilter.h"
+#include "itkDiscreteGaussianImageFilter.h"
+#include "itkGaussianOperator.h"
+#include "itkMacro.h"
+#include "itkResampleImageFilter.h"
+#include "itkShrinkImageFilter.h"
+#include "itkIdentityTransform.h"
+
+#include "itkMath.h"
+
+namespace itk
+{
+template <typename TInputImage, typename TOutputImage>
+VkMultiResolutionPyramidImageFilter<TInputImage, TOutputImage>::VkMultiResolutionPyramidImageFilter()
+{
+  m_KernelRadiusThreshold.Fill(10);
+}
+
+template <typename TInputImage, typename TOutputImage>
+void
+VkMultiResolutionPyramidImageFilter<TInputImage, TOutputImage>::GenerateData()
+{
+  // Get the input and output pointers
+  InputImageConstPointer inputPtr = this->GetInput();
+
+  // Create caster, smoother and resampleShrinker filters
+  using CasterType = CastImageFilter<TInputImage, TOutputImage>;
+
+  using ImageToImageType = ImageToImageFilter<TOutputImage, TOutputImage>;
+  using ResampleShrinkerType = ResampleImageFilter<TOutputImage, TOutputImage>;
+  using ShrinkerType = ShrinkImageFilter<TOutputImage, TOutputImage>;
+
+  auto caster = CasterType::New();
+  auto spatialSmoother = SpatialSmootherType::New();
+  auto fftSmoother = FFTSmootherType::New();
+  BaseSmootherType::Pointer smoother;
+
+  typename ImageToImageType::Pointer shrinkerFilter;
+  //
+  // only one of these pointers is going to be valid, depending on the
+  // value of UseShrinkImageFilter flag
+  typename ResampleShrinkerType::Pointer resampleShrinker;
+  typename ShrinkerType::Pointer         shrinker;
+
+  if (this->GetUseShrinkImageFilter())
+  {
+    shrinker = ShrinkerType::New();
+    shrinkerFilter = shrinker.GetPointer();
+  }
+  else
+  {
+    resampleShrinker = ResampleShrinkerType::New();
+    using LinearInterpolatorType = itk::LinearInterpolateImageFunction<OutputImageType, double>;
+    auto interpolator = LinearInterpolatorType::New();
+    resampleShrinker->SetInterpolator(interpolator);
+    resampleShrinker->SetDefaultPixelValue(0);
+    shrinkerFilter = resampleShrinker.GetPointer();
+  }
+  // Setup the filters
+  caster->SetInput(inputPtr);
+
+  spatialSmoother->SetUseImageSpacing(false);
+  spatialSmoother->SetInput(caster->GetOutput());
+  spatialSmoother->SetMaximumError(m_MaximumError);
+  fftSmoother->SetUseImageSpacing(false);
+  fftSmoother->SetInput(caster->GetOutput());
+  fftSmoother->SetMaximumError(m_MaximumError);
+
+  // shrinkerFilter->SetInput(smoother->GetOutput());
+
+  unsigned int ilevel, idim;
+  unsigned int factors[ImageDimension];
+  VarianceType   variance;
+  OutputSizeType     radius;
+  bool         useFFTSmoothing = false;
+
+  for (ilevel = 0; ilevel < m_NumberOfLevels; ++ilevel)
+  {
+    this->UpdateProgress(static_cast<float>(ilevel) / static_cast<float>(m_NumberOfLevels));
+
+    // Allocate memory for each output
+    OutputImagePointer outputPtr = this->GetOutput(ilevel);
+    outputPtr->SetBufferedRegion(outputPtr->GetRequestedRegion());
+    outputPtr->Allocate();
+
+    // compute shrink factors
+    for (idim = 0; idim < ImageDimension; ++idim)
+    {
+      factors[idim] = m_Schedule[ilevel][idim];
+    }
+
+    // compute kernel radius and select smoother type
+    // TODO clean up
+    radius = this->GetKernelRadius(ilevel);
+    if (m_KernelThresholdCondition == VkMultiResolutionPyramidImageFilterEnums::KernelThresholdType::ANY)
+    {
+      useFFTSmoothing = false;
+      for (idim = 0; idim < ImageDimension; ++idim)
+      {
+          if (radius[idim] >= m_KernelRadiusThreshold[idim])
+          {
+            useFFTSmoothing = true;
+            break;
+        }
+      }
+    }
+    else // ALL
+    {
+      useFFTSmoothing = true;
+      for (idim = 0; idim < ImageDimension; ++idim)
+      {
+        if (radius[idim] < m_KernelRadiusThreshold[idim])
+          {
+            useFFTSmoothing = false;
+            break;
+        }
+      }
+    }
+
+    // Compute
+
+    if (!this->GetUseShrinkImageFilter())
+    {
+      using IdentityTransformType = itk::IdentityTransform<double, OutputImageType::ImageDimension>;
+      auto identityTransform = IdentityTransformType::New();
+      resampleShrinker->SetOutputParametersFromImage(outputPtr);
+      resampleShrinker->SetTransform(identityTransform);
+    }
+    else
+    {
+      shrinker->SetShrinkFactors(factors);
+    }
+
+    // use mini-pipeline to compute output
+    smoother = (useFFTSmoothing ? fftSmoother : spatialSmoother);
+    variance = this->GetVariance(ilevel);
+    smoother->SetVariance(variance);
+    shrinkerFilter->SetInput(smoother->GetOutput());
+
+    shrinkerFilter->GraftOutput(outputPtr);
+
+    // force to always update in case shrink factors are the same
+    shrinkerFilter->Modified();
+    shrinkerFilter->UpdateLargestPossibleRegion();
+    this->GraftNthOutput(ilevel, shrinkerFilter->GetOutput());
+  }
+}
+
+template <typename TInputImage, typename TOutputImage>
+typename VkMultiResolutionPyramidImageFilter<TInputImage, TOutputImage>::OutputSizeType
+VkMultiResolutionPyramidImageFilter<TInputImage, TOutputImage>::GetKernelRadius(unsigned int ilevel) const
+{
+  using OperatorType = itk::GaussianOperator<OutputPixelType, ImageDimension>;
+  auto *   oper = new OperatorType;
+  OutputSizeType radius;
+  for (unsigned int dim = 0; dim < ImageDimension; ++dim)
+  {
+    oper->SetDirection(dim);
+    oper->SetMaximumError(m_MaximumError);
+    // oper->SetMaximumKernelWidth(32);
+    oper->SetVariance(this->GetVariance(ilevel)[dim]);
+    oper->CreateDirectional();
+    radius[dim] = oper->GetRadius()[dim];
+  }
+  return radius;
+}
+
+template <typename TInputImage, typename TOutputImage>
+typename VkMultiResolutionPyramidImageFilter<TInputImage, TOutputImage>::VarianceType
+VkMultiResolutionPyramidImageFilter<TInputImage, TOutputImage>::GetVariance(unsigned int ilevel) const
+{
+  VarianceType variance;
+  for (unsigned int dim = 0; dim < ImageDimension; ++dim)
+  {
+    variance[dim] = itk::Math::sqr(0.5 * static_cast<float>(m_Schedule[ilevel][dim]));
+  }
+  return variance;
+}
+
+/**
+ * PrintSelf method
+ */
+template <typename TInputImage, typename TOutputImage>
+void
+VkMultiResolutionPyramidImageFilter<TInputImage, TOutputImage>::PrintSelf(std::ostream & os, Indent indent) const
+{
+  Superclass::PrintSelf(os, indent);
+
+  os << indent << "Kernel radius threshold: " << m_KernelRadiusThreshold << std::endl;
+  os << indent << "Kernel threshold condition: " << m_KernelThresholdCondition << std::endl;
+}
+} // namespace itk
+
+#endif // itkVkMultiResolutionPyramidImageFilter_hxx
---
+++ test/itkVkMultiResolutionPyramidImageFilterTest.cxx
@@ -0,0 +1,131 @@
+/*=========================================================================
+ *
+ *  Copyright NumFOCUS
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+
+#include <iostream>
+
+#include "itkVkMultiResolutionPyramidImageFilter.h"
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+#include "itkMath.h"
+#include "itkTestingMacros.h"
+
+namespace
+{
+// The following three classes are used to support callbacks
+// on the filter in the pipeline that follows later
+class ShowProgressObject
+{
+public:
+  ShowProgressObject(itk::ProcessObject * o) { m_Process = o; }
+  void
+  ShowProgress()
+  {
+    std::cout << "Progress " << m_Process->GetProgress() << std::endl;
+  }
+  itk::ProcessObject::Pointer m_Process;
+};
+} // namespace
+
+int
+itkVkMultiResolutionPyramidImageFilterTest(int argc, char * argv[])
+{
+  if (argc < 3)
+  {
+    std::cerr << "Missing Parameters." << std::endl;
+    std::cerr << "Usage: " << itkNameOfTestExecutableMacro(argv);
+    std::cerr << " inputImage outputImage [kernelCondition] [useShrinkFilter] [numLevels]" << std::endl;
+    std::cerr << std::flush;
+    return EXIT_FAILURE;
+  }
+
+  constexpr unsigned int ImageDimension = 2;
+  using InputPixelType = float;
+  using ImageType = itk::Image<InputPixelType, ImageDimension>;
+
+  auto inputImage = itk::ReadImage<ImageType>(argv[1]);
+
+  auto kernelCondition =
+    (argc > 3 && std::atoi(argv[3]) == 1 ? itk::VkMultiResolutionPyramidImageFilterEnums::KernelThresholdType::ALL
+                                         : itk::VkMultiResolutionPyramidImageFilterEnums::KernelThresholdType::ANY);
+  bool useShrinkFilter = (argc > 4 && std::atoi(argv[4]) == 1);
+  unsigned int numLevels = (argc > 5 ? std::atoi(argv[5]) : 3);
+
+  // Set up multi-resolution pyramid
+  using PyramidType = itk::VkMultiResolutionPyramidImageFilter<ImageType, ImageType>;
+  using ScheduleType = PyramidType::ScheduleType;
+
+  auto pyramidFilter = PyramidType::New();
+  pyramidFilter->SetInput(inputImage);
+  pyramidFilter->SetUseShrinkImageFilter(useShrinkFilter);
+
+  // Use default schedule
+  pyramidFilter->SetNumberOfLevels(numLevels);
+
+  // TODO set schedule by specifying the starting shrink factors
+  /*numLevels = 4;
+  factors[0] = 8;
+  factors[1] = 4;
+  factors[2] = 2;
+  pyramid->SetNumberOfLevels(numLevels);
+  pyramid->SetStartingShrinkFactors(factors.Begin());*/
+
+  // TODO test get variance
+  for (unsigned int level = 0; level < numLevels; ++level)
+  {
+    auto schedule = pyramidFilter->GetSchedule();
+    auto variance = pyramidFilter->GetVariance(level);
+    auto radius = pyramidFilter->GetKernelRadius(level);
+
+    for (unsigned int dim = 0; dim < ImageDimension; ++dim)
+    {
+      ITK_TEST_EXPECT_TRUE(itk::Math::AlmostEquals(variance[dim], itk::Math::sqr(0.5 * schedule[level][dim])));
+      // Full calculations for default Gaussian size are outside the scope of this test
+      // so just test that radius decreases with level
+      if (level > 0)
+      {
+        ITK_TEST_EXPECT_TRUE(radius < pyramidFilter->GetKernelRadius(level - 1));
+      }
+    }
+  }
+
+  // TODO test get kernel radius
+
+  // TODO test using recursive filter for additional speedup
+  ITK_EXERCISE_BASIC_OBJECT_METHODS(
+    pyramidFilter, VkMultiResolutionPyramidImageFilter, MultiResolutionPyramidImageFilter);
+
+  ShowProgressObject                                    progressWatch(pyramidFilter);
+  itk::SimpleMemberCommand<ShowProgressObject>::Pointer command;
+  command = itk::SimpleMemberCommand<ShowProgressObject>::New();
+  command->SetCallbackFunction(&progressWatch, &ShowProgressObject::ShowProgress);
+  pyramidFilter->AddObserver(itk::ProgressEvent(), command);
+
+  pyramidFilter->Update();
+
+  //  update pyramid at a particular level
+  /*for (unsigned int testLevel = 0; testLevel < numLevels; ++testLevel)
+  {
+    pyramid->GetOutput(testLevel)->Update();
+  }*/
+  for (unsigned int ilevel = 0; ilevel < numLevels; ++ilevel)
+  {
+    itk::WriteImage(pyramidFilter->GetOutput(ilevel), argv[2] + std::to_string(ilevel) + ".mhd");
+  }
+
+  return EXIT_SUCCESS;
+}
