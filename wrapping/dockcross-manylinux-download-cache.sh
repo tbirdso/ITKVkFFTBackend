30c966bc723daf5cfaccb95bb747e95ee135775f
Tom Birdsong <tom.birdsong@kitware.com>
2022-12-01 09:00:11 -0500

    ENH: Bump for ITK v5.3.0

--- wrapping/dockcross-manylinux-download-cache.sh
+++ wrapping/dockcross-manylinux-download-cache.sh
@@ -2,6 +2,10 @@
 
 # This module should be pulled and run from an ITKModule root directory to generate the Linux python wheels of this module,
 # it is used by the circle.yml file contained in ITKModuleTemplate: https://github.com/InsightSoftwareConsortium/ITKModuleTemplate
+#
+# Forked from
+# https://github.com/InsightSoftwareConsortium/ITKPythonPackage/blob/master/scripts/dockcross-manylinux-download-cache-and-build-module-wheels.sh
+
 
 curl https://data.kitware.com/api/v1/file/592dd8068d777f16d01e1a92/download -o zstd-1.2.0-linux.tar.gz
 gunzip -d zstd-1.2.0-linux.tar.gz
@@ -13,6 +17,20 @@ curl -L https://github.com/InsightSoftwareConsortium/ITKPythonBuilds/releases/do
 echo "Extracting all files"
 tar xf ${TARBALL_NAME}
 rm ${TARBALL_NAME}
+
+# Optional: Update build scripts
+if [[ -n ${ITKPYTHONPACKAGE_TAG} ]]; then
+  echo "Updating build scripts to ${ITKPYTHONPACKAGE_ORG:=InsightSoftwareConsortium}/ITKPythonPackage@${ITKPYTHONPACKAGE_TAG}"
+  git clone "https://github.com/${ITKPYTHONPACKAGE_ORG}/ITKPythonPackage.git" "IPP-tmp"
+  pushd IPP-tmp/
+  git checkout "${ITKPYTHONPACKAGE_TAG}"
+  git status
+  popd
+
+  rm -rf ITKPythonPackage/scripts/
+  cp -r IPP-tmp/scripts ITKPythonPackage/
+  rm -rf IPP-tmp/
+fi
 
 mkdir tools
 curl https://data.kitware.com/api/v1/file/5c0aa4b18d777f2179dd0a71/download -o doxygen-1.8.11.linux.bin.tar.gz
